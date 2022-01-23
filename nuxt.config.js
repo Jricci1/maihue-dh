2a3cce9c78aeb7474f94a634120da147dfdf50fb
jricci1 <joaquin.ricci@thenotcompany.com>
2022-01-23 19:11:44 -0300

    Fix linter errors

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,12 +1,12 @@
 export default {
-    // Global page headers (https://go.nuxtjs.dev/config-head)
-    head: {
-        title: 'Maihue DH',
-        meta: [
-            { charset: 'utf-8' },
-            { name: 'viewport', content: 'width=device-width, initial-scale=1' }
-        ]
-    },
-    // Source directory of the Nuxt application.
-    srcDir: 'src/'
-}
+  // Global page headers (https://go.nuxtjs.dev/config-head)
+  head: {
+    title: "Maihue DH",
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+    ],
+  },
+  // Source directory of the Nuxt application.
+  srcDir: "src/",
+};
