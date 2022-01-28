fb2ec452a42c5629d20cd0c978132b15cde2c89c
jricci1 <joaquin.ricci@thenotcompany.com>
2022-01-28 02:32:48 -0300

    Improve ScoreBoardForm to show errors, load image and delete elements on submit

--- src/components/ScoreBoardForm.vue
+++ src/components/ScoreBoardForm.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-form>
+  <v-form ref="form">
     <v-container>
       <v-row>
         <v-col cols="12" md="2">
@@ -7,7 +7,6 @@
             v-model="name"
             :counter="15"
             label="First name"
-            required
           ></v-text-field>
         </v-col>
 
@@ -16,7 +15,6 @@
             v-model="nickName"
             :counter="10"
             label="Nickname"
-            required
           ></v-text-field>
         </v-col>
 
@@ -25,7 +23,6 @@
             v-model="lastName"
             :counter="10"
             label="Last name"
-            required
           ></v-text-field>
         </v-col>
 
@@ -33,6 +30,7 @@
           <v-text-field
             v-model="category"
             :counter="10"
+            :rules="categoryRules"
             label="Category"
             type="number"
             required
@@ -41,10 +39,12 @@
 
         <v-col cols="12" md="2">
           <v-file-input
-            accept="image/png, image/jpeg, image/bmp"
+            ref="fileUpload"
+            accept="image/gif, image/jpg, image/jpeg, image/png"
             placeholder="Pick an avatar"
             prepend-icon="mdi-camera"
             label="Avatar"
+            @change="createAvatarLocalUrl"
           ></v-file-input>
         </v-col>
 
@@ -65,27 +65,69 @@
 <script setup>
 import { toRefs, reactive } from "@vue/composition-api";
 import useStore from "~/compositions/useStore";
+import useNotify from "~/compositions/useNotify";
+
+const { notify } = useNotify();
 
 const { commit } = useStore("scoreBoard");
 
-const { name, nickName, lastName, avatar, category } = toRefs(
+const { form, fileUpload, name, nickName, lastName, avatar, category } = toRefs(
   reactive({
+    form: null,
+    fileUpload: null,
     name: null,
     nickName: null,
-    amount: null,
     lastName: null,
     category: null,
     avatar: null,
   })
 );
 
+const categoryRules = [(v) => !!v || "Required field"];
+
+const validateForm = () => {
+  console.log(form.value.validate());
+  if (form.value.validate()) {
+    if (!(name.value || nickName.value || lastName.value)) {
+      notify.info("Please provide a Name, Nickname or Last name.");
+      return false;
+    }
+    return true;
+  }
+  return false;
+};
+
+const createAvatarLocalUrl = (file) => {
+  console.log(file);
+  if (!file) {
+    avatar.value = null;
+    return;
+  }
+  const imageUrl = URL.createObjectURL(file);
+  console.log(imageUrl);
+  avatar.value = imageUrl;
+};
+
 const addNewParticipantToBoard = () => {
-  commit("addParticipant", {
-    name: name.value,
-    nickName: nickName.value,
-    lastName: lastName.value,
-    avatar: avatar.value,
-    category: category.value,
-  });
+  if (validateForm()) {
+    commit("addParticipant", {
+      name: name.value,
+      nickName: nickName.value,
+      lastName: lastName.value,
+      avatar: avatar.value,
+      category: category.value,
+    });
+    resetForm();
+    form.value.resetValidation();
+  }
+};
+
+const resetForm = () => {
+  name.value = null;
+  nickName.value = null;
+  lastName.value = null;
+  avatar.value = null;
+  category.value = null;
+  fileUpload.value.reset();
 };
 </script>
