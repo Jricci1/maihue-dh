0c73c6570e501e7f462310e40777cdaa8cb1425f
Cristián Aguirre <cristian.aguirre@imfd.cl>
2022-01-27 23:15:16 -0300

    Add properies to the documentation

--- src/store/scoreBoard/state.js
+++ src/store/scoreBoard/state.js
@@ -4,6 +4,10 @@
  * and their information. The object keys are the participant IDs and the values are the participants.
  * @property {Array} participantsOrder List of participant IDs sorted by their order on the scoreboard.
  * The order should be either by their time (ascending) or by their adition order.
+ * @property {str} currentRider ID of the current rider who is going to compete now
+ * @property {str} hotSeat ID of the best rider of the same category of the current rider
+ * @property {Boolean} ongoingRace If any race is ongoing. This parameter is use to avoid change
+ * the current rider if is riding
  */
 
 /**
