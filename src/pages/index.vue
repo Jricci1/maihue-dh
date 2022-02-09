b4a8b4b6b9651677a7a253e5e69cae3cb8a89a2a
jricci1 <joaquin.ricci@thenotcompany.com>
2022-02-09 02:16:38 -0300

    Add maihue logo on the background

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,16 +1,34 @@
 <template>
-  <main class="d-flex fill-height d-flex flex-column">
-    <v-container>
-      <h1>Maihue - DH</h1>
-    </v-container>
-    <div class="d-flex justify-space-around">
+  <main class="d-flex fill-height d-flex flex-column maihue-title mt-3">
+    <img :src="require('~/assets/img/Logo.jpeg')" aspect-ratio="2" />
+    <div class="d-flex justify-space-around mayor-index">
       <RiderCard :hot-seat="true" />
       <ScoreTimer />
       <RiderCard />
     </div>
     <v-spacer />
-    <section>
+    <section class="mayor-index">
       <ScoreBoard />
     </section>
   </main>
 </template>
+
+<style scoped>
+.maihue-title {
+  position: relative;
+  overflow: hidden;
+  padding: 16px;
+}
+.maihue-title img {
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: auto;
+  opacity: 0.3;
+}
+
+.mayor-index {
+  z-index: 4;
+}
+</style>
