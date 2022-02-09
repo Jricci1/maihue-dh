9dd0e273667da332a13c0c3f484f16456ca39564
jricci1 <joaquin.ricci@thenotcompany.com>
2022-02-09 02:16:20 -0300

    Add cuchu to collaborators

--- src/components/layouts/AppFooter.vue
+++ src/components/layouts/AppFooter.vue
@@ -1,7 +1,7 @@
 <template>
   <v-footer app padless>
     <v-container fluid class="text-center purple">
-      <span>JRP &copy; {{ new Date().getFullYear() }}</span>
+      <span>JRP/CDA &copy; {{ new Date().getFullYear() }}</span>
     </v-container>
   </v-footer>
 </template>
