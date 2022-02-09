f11b1ab7552e0e3b0bfa12d7af97e9dff000e68f
jricci1 <joaquin.ricci@thenotcompany.com>
2022-02-09 02:21:35 -0300

    Set footer absolute

--- src/components/layouts/AppFooter.vue
+++ src/components/layouts/AppFooter.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-footer app padless>
+  <v-footer app padless absolute>
     <v-container fluid class="text-center purple">
       <span>JRP/CDA &copy; {{ new Date().getFullYear() }}</span>
     </v-container>
