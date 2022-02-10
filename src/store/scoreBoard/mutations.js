a1c217166ee87dbdfd6e99ce2e624b4e02054428
jricci1 <joaquin.ricci@thenotcompany.com>
2022-02-10 11:59:25 -0300

    Add deleteParticipant mutation

--- src/store/scoreBoard/mutations.js
+++ src/store/scoreBoard/mutations.js
@@ -2,7 +2,7 @@
  * A module of providers store with the mutations to modify the state of the
  * score board.
  */
-
+import Vue from "vue";
 import { generateUUID } from "~/utils";
 
 /**
@@ -14,6 +14,7 @@ import { generateUUID } from "~/utils";
  * Check the state documentation for more details about it.
  *
  * @param {Object} state The state of the scoreBoard module.
+ * @param {Object} newParticipant The participant to add.
  */
 const addParticipant = (state, newParticipant) => {
   const newParticipantId = generateUUID();
@@ -27,12 +28,25 @@ const addParticipant = (state, newParticipant) => {
  * Add participant's time into the score board.
  *
  * @param {Object} state The state of the scoreBoard module.
+ * @param {Object} payload
+ * @param {Object} payload.participantId The ID of the participant to set the time.
+ * @param {Object} payload.time The time to add to the participant.
  */
 const addParticipantTime = (state, { participantId, time }) => {
   state.participants[participantId].time = time;
 };
 
+/**
+ * Delete participant from the score board.
+ *
+ * @param {Object} state The state of the scoreBoard module.
+ */
+const deleteParticipant = (state, { participantId }) => {
+  Vue.delete(state.participants, participantId);
+};
+
 export default {
   addParticipant,
   addParticipantTime,
+  deleteParticipant,
 };
